4ecc6c792795629ed0b62354fcae291b5d58ff88
nareshved <nkraj021@gmail.com>
2024-09-20 18:08:35 +0530

    check

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       title: 'employee_management',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
         useMaterial3: true,
       ),
       home: const HomePage(),
